9fdbd6d79d3a063aace8f719c53032dddbce2a3b
SumitRaj18 <sumitraj7162@gmail.com>
2024-10-23 21:00:09 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+       'primary':'#30146c',
+       'secondary':"#f0ecf4"
+      },
+      fontFamily:{
+        'Ag':'Agdasima'
+      }
+    },
+  },
+  plugins: [],
+}
+
